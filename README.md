0341c134283060c191f0d7772ac003016e1aaaaf
J-rod-mech <jtn033@ucsd.edu>
2026-05-05 00:06:26 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
+
 Jason Nguyen
+
+1. I wouldn't use a unit test to test the "message" feature because this likely involves multiple interacting components for the sender and recipient, making it better observed on the application level.
+
+2. I would use a unit test to test the "max message length" feature because it should be an isolated problem to a single message component.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,44 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  function loadVoices() {
+    const synth = window.speechSynthesis;
+    const voices = synth.getVoices();
+    for (const voice of voices) {
+      const option = document.createElement('option');
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      document.getElementById('voice-select').appendChild(option);
+    }
+  }
+
+  document.querySelector('button').addEventListener('click', function() {
+    const synth = window.speechSynthesis;
+    const text = document.getElementById('text-to-speak').value;
+    const utterance = new SpeechSynthesisUtterance(text);
+    const selectedOption = 
+      document.getElementById('voice-select').selectedOptions[0].getAttribute('data-name');
+    const voices = synth.getVoices();
+    for (const voice of voices) {
+      if (voice.name === selectedOption) {
+        utterance.voice = voice;
+        break;
+      }
+    }
+    synth.speak(utterance);
+    document.querySelector('img').src = 'assets/images/smiling-open.png';
+    utterance.onend = function() {
+      document.querySelector('img').src = 'assets/images/smiling.png';
+    };
+  });
+
+  document.getElementById('voice-select').addEventListener('change', function() {
+    const synth = window.speechSynthesis;
+    synth.cancel();
+    document.querySelector('img').src = 'assets/images/smiling.png';
+  });
+
+  loadVoices();
+  speechSynthesis.onvoiceschanged = loadVoices;
 }
